e1f8798f145efbcbaee70262843961bba05579a4
ZetaKebab <me@theomarchal.com>
2017-05-10 02:19:20 +0200

    app run explanations

--- required_example.js
+++ required_example.js
@@ -1,3 +1,13 @@
+/*
+** To use this bot, you should do the following things:
+** - install nodeJS, npm, and discord.js
+** - rename this file `required.js`
+** - create a bot on your discord page
+** - replace the secret token key with the one the app
+** - run the bot by executing `node aubergine.js`
+** enjoy this stupid thing...
+*/
+
 exports.startAubergine = function() {
 
 	const Discord = require("discord.js");
